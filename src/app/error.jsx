57ccc2e44731d6a4489588b3f757788e2304005a
Mahi <maheshlucky90926@gmail.com>
2024-04-05 12:04:15 +0530

    added loadiing component in nextjs

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -7,7 +7,7 @@ const error = ({error,reset}) => {
   return (
     <div className=" text-center mt-16">
 <h1>something went wrong  </h1>
-<button className=" hover:text-blue-600" onClick={()=>reset()}>Try again</button>
+<button className="h-52 hover:text-blue-600" onClick={()=>reset()}>Try again</button>
 
     </div>
   )
---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center'>
+        <img src='spinner.svg' alt='Loading...'/>
+
+    </div>
+  )
+}
+
+export default loading 
